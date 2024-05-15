923cf7bdd3772bd5ae76464038d4f16149f377e3
omar jandali <omarjandali93@gmail.com>
2024-05-14 20:42:21 -0700

    Omar Jandali.  updated the PostTiles

--- src/Screens/Feed/FeedScreen.tsx
+++ src/Screens/Feed/FeedScreen.tsx
@@ -45,7 +45,6 @@ const FeedScreen = () => {
   return (
     <View className={'flex-1 bg-neutral-900'}>
       <FeedHeader />
-      <Text className={'text-white font-bold'}>Screen</Text>
       <TouchableOpacity onPress={() => {signOutUser()}}><Text className={'text-white font-bold'}>Logout</Text></TouchableOpacity>
       {
         loggedInUsersPosts && loggedInUsersPosts.length > 0
