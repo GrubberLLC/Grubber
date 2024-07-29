3b335558adbe94f118cbf9e450486e0e65a565d0
omar jandali <omarjandali93@gmail.com>
2024-07-29 15:51:01 -0700

    Omar Jandali. trying to fix location permission issue

--- src/Screens/Search/SearchScreen.tsx
+++ src/Screens/Search/SearchScreen.tsx
@@ -1,15 +1,11 @@
 import React, { useEffect, useState } from 'react';
-import { Text, View, PermissionsAndroid, Platform, Alert, TouchableOpacity } from 'react-native';
+import { Text, View, Alert, TouchableOpacity } from 'react-native';
 import SearchBarComponent from '../../Components/Search/SearchBarComponent';
 import IconLoadingButtonComponent from '../../Components/Buttons/IconLoadingButtonComponent';
 import { useSearch } from '../../Context/SearchContext';
 import { ScrollView } from 'react-native-gesture-handler';
 import PlaceTileYelpNoSelect from '../../Components/Tile/PlaceTileYelpNoSelect';
 import ColorGuide from '../../ColorGuide';
-import Geolocation from '@react-native-community/geolocation';
-import { check, request, PERMISSIONS, RESULTS } from 'react-native-permissions';
-import Geocoder from 'react-native-geocoding';
-import { GOOGLE_API } from '../../API/Authorizatgion';
 import { List, MapPin } from 'react-native-feather';
 import MapView, { Marker, Callout } from 'react-native-maps';
 import PlaceTileMap from '../../Components/Tile/PlaceTileMap';
@@ -28,48 +24,10 @@ const SearchScreen = () => {
     mapLatitude, setMapLatitude
   } = useSearch();
 
-  const getLocationAndSearch = () => {
-    Geolocation.getCurrentPosition(
-      (position) => {
-        const { latitude, longitude } = position.coords;
-        setMapLatitude(latitude);
-        setMapLongitude(longitude);
-        Geocoder.from({ latitude, longitude })
-          .then((response) => {
-            updateLocationSearch(response.results[0]['address_components'][3]['short_name']);
-          })
-          .catch((error) => {
-            console.log('error getting location: ', error);
-          });
-        searchYelp(longitude, latitude);
-      },
-      (error) => {
-        console.error(error);
-        if (error.code === 1) {
-          Alert.alert('Permission Denied', 'Location permission is denied.');
-        } else if (error.code === 2) {
-          Alert.alert('Position Unavailable', 'Unable to determine location.');
-        } else if (error.code === 3) {
-          Alert.alert('Timeout', 'Unable to fetch location within the specified time.');
-        } else {
-          Alert.alert('Error', 'Unable to fetch location.');
-        }
-        updateLocationSearch('Los Angeles, CA');
-        searchYelp();
-      },
-      { enableHighAccuracy: true, timeout: 30000, maximumAge: 10000 }
-    );
-  };
-
-  // useEffect(() => {
-  //   requestLocationPermission().then(() => {
-  //     if (locationGranted) {
-  //       getLocationAndSearch();
-  //     } else {
-  //       updateLocationSearch('Los Angeles, CA');
-  //     }
-  //   });
-  // }, [locationGranted]);
+  useEffect(() => {
+    updateLocationSearch('Los Angeles, CA');
+    searchYelp();
+  }, []);
 
   return (
     <View className='flex-1' style={{ backgroundColor: ColorGuide['bg-dark'] }}>
